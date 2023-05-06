f8375f5543ee7176bed0fd2c8a50d6d69cdab8ee
RhmdAri <rhmdari1907@gmail.com>
2023-05-06 12:13:44 +0800

    Update DoWhile.java
    baris kode dowhile yang lengkap

--- pbo1-ptm04/src/main/java/loops/DoWhile.java
+++ pbo1-ptm04/src/main/java/loops/DoWhile.java
@@ -10,8 +10,18 @@ package loops;
  */
 public class DoWhile {
     public static void main(String[] args) {
+       int isiGelas=0;
+       int penuh=225;
+        
+        System.out.println("Isi Gelas Sekarang : " + isiGelas + "ml");
+        System.out.println("Isi Gelas Ketika Penuh : " + penuh + "ml");
+        
         do {
-            isiAir();
+        isiGelas++;
+        System.out.println("Sedang mengisi gelas...");
+        System.out.println("Isi gelas sekarang : " + isiGelas + "ml");
         }while(isiGelas != penuh);
+        
+        System.out.println("Finale : Isi gelas sekarang : " + isiGelas + "ml");
     }
 }
