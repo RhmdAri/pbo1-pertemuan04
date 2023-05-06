f5f4b2bada32bbb910d91ac1ea548d50a3d9e519
RhmdAri <rhmdari1907@gmail.com>
2023-05-06 12:34:53 +0800

    Create Tugas.java
    Tugas Array

---
+++ pbo1-ptm04/src/main/java/array/Tugas.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package array;
+
+/**
+ *
+ * @author Welcomp
+ */
+public class Tugas {
+     public static void main (String args[])
+   {
+    //Deklarasi array
+    String[] mahasiswa = {"Rini","Aldi","Acep","Putra"};
+
+    //Menampilkan isi array pada indeks ke-2
+    System.out.println(mahasiswa[2]);
+         
+    }
+}
