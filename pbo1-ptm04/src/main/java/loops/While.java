09caf67c1374fa74b6669dfaf1d87e4108a1453d
RhmdAri <rhmdari1907@gmail.com>
2023-05-06 12:07:05 +0800

    Create While.java
    loops while dengan baris kode belum lengkap

---
+++ pbo1-ptm04/src/main/java/loops/While.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package loops;
+
+/**
+ *
+ * @author Welcomp
+ */
+public class While {
+    public static void main(String[] args) {
+        while(isiGelas != penuh){
+            isiAir();
+        }
+    }
+}
