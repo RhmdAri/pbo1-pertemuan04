56c7b2b9c751c86677ea37e7b0e4e062029da315
RhmdAri <rhmdari1907@gmail.com>
2023-05-06 12:22:40 +0800

    Create ArrString.java
    Array String

---
+++ pbo1-ptm04/src/main/java/array/ArrString.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package array;
+
+/**
+ *
+ * @author Welcomp
+ */
+public class ArrString {
+    public static void main(String[] args) {
+        String mataKuliah[];
+        mataKuliah = new String[10];
+        
+        mataKuliah[0] = "Pemrograman Berbasis Objek";
+        mataKuliah[1] = "Visual";
+        mataKuliah[2] = "Rekayasa Perangkat Lunak";
+        mataKuliah[3] = "Organisasi Komputer";
+        mataKuliah[4] = "Pemrograman Web 2";
+    }
+}
