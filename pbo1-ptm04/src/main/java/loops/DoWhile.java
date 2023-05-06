5903c6ef35fba9e5bed9474c25fa532b340a66ed
RhmdAri <rhmdari1907@gmail.com>
2023-05-06 12:12:24 +0800

    Create DoWhile.java
    dowhile yang belum lengkap

---
+++ pbo1-ptm04/src/main/java/loops/DoWhile.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package loops;
+
+/**
+ *
+ * @author Welcomp
+ */
+public class DoWhile {
+    public static void main(String[] args) {
+        do {
+            isiAir();
+        }while(isiGelas != penuh);
+    }
+}
